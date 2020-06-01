410d66234ecc83dd61289433367bfc5750052b57
Adam Thorpe <adam.thorpe@ibm.com>
2020-06-01 14:14:47 +0100

    Removed localhost reference

--- src/api.js
+++ src/api.js
@@ -1,10 +1,7 @@
 import { get } from "./utils";
 
-// var address = "https://api.adoptopenjdk.net/"
-var address = "http://localhost:8080/"
-
 export const api = {
-    downloads: (jdkVersion = "") => get(`${address}v3/stats/downloads/total/${jdkVersion}`),
-    tracking: ({ source, feature_version, jvm_impl, days, ...rest }) => get(`${address}v3/stats/downloads/tracking/`, { source, feature_version, jvm_impl, days, ...rest }),
-    monthly: ({ source, feature_version, jvm_impl, ...rest }) => get(`${address}v3/stats/downloads/monthly/`, { source, feature_version, jvm_impl, ...rest })
+    downloads: (jdkVersion = "") => get(`https://api.adoptopenjdk.net/v3/stats/downloads/total/${jdkVersion}`),
+    tracking: ({ source, feature_version, jvm_impl, days, ...rest }) => get(`https://api.adoptopenjdk.net/v3/stats/downloads/tracking/`, { source, feature_version, jvm_impl, days, ...rest }),
+    monthly: ({ source, feature_version, jvm_impl, ...rest }) => get(`https://api.adoptopenjdk.net/v3/stats/downloads/monthly/`, { source, feature_version, jvm_impl, ...rest })
 };
